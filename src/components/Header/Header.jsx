5856b458256266fd7ec6341debe9cd7af3015e5b
Sushmita Kumari <sushmitajgi543@gmail.com>
2022-08-08 14:33:12 +0530

    added animation

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -26,21 +26,22 @@ function Header() {
               </p>
             </div>
             <div className='header-container-content-right'>
-              <Navbar key="lg" expand="false" className="mb-3">
+              <Navbar key="lg" expand="false" className="mb-3" >
                 <Container fluid id='container'>
                   <Navbar.Toggle id="toggle" aria-controls={`offcanvasNavbar-expand-lg`} />
                   <Navbar.Offcanvas
+                  className="offcanvas-navbar"
                     id={`offcanvasNavbar-expand-lg`}
                     aria-labelledby={`offcanvasNavbarLabel-expand-lg`}
                     placement="end"
                   >
-                    <Offcanvas.Header className='close' closeButton>
+                    <Offcanvas.Header id='close' closeButton>
                       <Offcanvas.Title id={`offcanvasNavbarLabel-expand-lg`}>
-                        Offcanvas
+                        
                       </Offcanvas.Title>
                     </Offcanvas.Header>
                     <Offcanvas.Body>
-                      <Nav className="justify-content-end flex-grow-1 pe-3">
+                      <Nav  id="nav-list" className="justify-content-end flex-grow-1 pe-3">
                         <Link className='nav-a' to="/">  <Nav.Link  className='nav-a'  href="#action1">Home</Nav.Link></Link>
                         <Link className='nav-a' to="/project"><Nav.Link  className='nav-a' href="#action2">Projects</Nav.Link></Link>
                         <Link className='nav-a' to="/about-us"><Nav.Link  className='nav-a' href="#action2">Prime Story</Nav.Link></Link>
